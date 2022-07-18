f8ad15b6b1477b11a18cddee878d785fd2642c68
Quoc Hung <hugn.nq1909@gmail.com>
2022-07-18 16:39:07 +0700

    Change to GG Map API

--- HoanKiem Air Application/app/src/main/java/com/example/hoankiemaircontrol/ui/MainActivity.java
+++ HoanKiem Air Application/app/src/main/java/com/example/hoankiemaircontrol/ui/MainActivity.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 
-import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
 
 import com.example.hoankiemaircontrol.R;
@@ -28,14 +27,19 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
 
 import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
 import java.util.ArrayList;
+import java.util.List;
 
 import info.hoang8f.android.segmented.SegmentedGroup;
 
-public class MainActivity extends BaseActivity implements IMessageListener{
+public class MainActivity extends BaseActivity implements IMessageListener, OnMapReadyCallback {
 
     private final static int DISPLAY_MODE_TRAFFIC = 0;
     private final static int DISPLAY_MODE_POLLUTION = 1;
@@ -51,12 +55,19 @@ public class MainActivity extends BaseActivity implements IMessageListener{
     LineChart lineChart;
     LineData lineData;
     LineDataSet lineDataSet;
-    Button mapButton;
 
 
     private static String[] mess2;
     static ArrayList<Entry> lineEntries = new ArrayList<>();
     private static String ip;
+
+    private GoogleMap mMap;
+    Button draw_polygon;
+    Button clear;
+    Polygon polygon, polygon1;
+
+    private final List<LatLng> latLngList = new ArrayList<>();
+    private final List<Marker> markerList = new ArrayList<>();
 
 
     @Override
@@ -67,14 +78,20 @@ public class MainActivity extends BaseActivity implements IMessageListener{
 
         ip = getIntent().getStringExtra("ip");
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        if (mapFragment != null){
+            mapFragment.getMapAsync(MainActivity.this);
+        }
+
         // Catch UI
         mSeekBarNumCars = findViewById(R.id.seekBar_for_cars);
         mSeekBarNumMotorbikes = findViewById(R.id.seekBar_for_motobikes);
-        mRadioGroupRoadScenario = findViewById(R.id.radio_group_road_scenario);
         mRadioGroupDisplayMode = findViewById(R.id.radio_group_display_mode);
-        mapButton = findViewById(R.id.Button_changetoMap);
-        mapButton.setOnClickListener(this::onClick);
-
+        draw_polygon = findViewById(R.id.draw_polygon);
+        draw_polygon.setOnClickListener(this::drawPolygon);
+        clear = findViewById(R.id.clear);
+        clear.setOnClickListener(this::drawClear);
 
         // Handle uncaught exception
         Thread.setDefaultUncaughtExceptionHandler(
@@ -94,6 +111,61 @@ public class MainActivity extends BaseActivity implements IMessageListener{
     }
 
 
+
+    private void drawPolygon(View view) {
+        if(polygon != null){
+            polygon.remove();
+        }
+        PolygonOptions polygonOptions = new PolygonOptions().addAll(latLngList).clickable(true)
+                .strokeColor(Color.BLUE).strokeWidth(3);
+        polygon = mMap.addPolygon(polygonOptions);
+
+        TCP.getInstance(MainActivity.this).SendMessageTask("Block_of_polygon", latLngList);
+    }
+
+    private void drawClear(View view){
+        if(polygon != null) polygon.remove();
+        for(Marker marker : markerList) marker.remove();
+        latLngList.clear();
+        markerList.clear();
+
+        TCP.getInstance(MainActivity.this). SendMessageTask("Block_of_polygon", latLngList);
+    }
+
+    public void setBounder(){
+        double top = 21.04287722989717;
+        double right = 105.87771067295719;
+        double bottom = 21.02153486576196;
+        double left = 105.83025731798917;
+
+        LatLngBounds bounds = new LatLngBounds(
+                new LatLng(bottom, left),
+                new LatLng(top, right)
+        );
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 1150, 1150, 0));
+        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(MainActivity.this, R.raw.map_style));
+
+        polygon1 = mMap.addPolygon(new PolygonOptions()
+                .add(new LatLng(21.020657, 105.841596),
+                        new LatLng(21.028989, 105.841660),
+                        new LatLng(21.029189, 105.842561),
+                        new LatLng(21.028788, 105.843012),
+                        new LatLng(21.031923, 105.844353),
+                        new LatLng(21.032364, 105.843409),
+                        new LatLng(21.035087, 105.843945),
+                        new LatLng(21.034862, 105.845008),
+                        new LatLng(21.039913, 105.846125),
+                        new LatLng(21.040834, 105.850320),
+                        new LatLng(21.040284, 105.850792),
+                        new LatLng(21.042867, 105.857498),
+                        new LatLng(21.020684, 105.868289),
+                        new LatLng(21.018430, 105.861101),
+                        new LatLng(21.019071, 105.858746),
+                        new LatLng(21.018370, 105.855077),
+                        new LatLng(21.017960, 105.853918))
+                .strokeColor(Color.RED).strokeWidth(3));
+    }
 
     // Function for handle message that app receive
 
@@ -137,26 +209,26 @@ public class MainActivity extends BaseActivity implements IMessageListener{
     }
 
     // Button change status of road
-    public void onRoadScenarioRadioButtonClicked(View v) {
-        boolean checked = ((RadioButton) v).isChecked();
-        // Check which radio button was clicked
-        switch(v.getId()) {
-            case R.id.radio_button_scenario_0:
-                if (checked)
-                    TCP.getInstance(MainActivity.this).SendMessageTask("road_scenario", NO_CLOSE_ROADS);
-
-                break;
-            case R.id.radio_button_scenario_1:
-                if (checked)
-                    TCP.getInstance(MainActivity.this).SendMessageTask("road_scenario", PEDESTRIAN_ZONE_ACTIVE);
-                break;
-            case R.id.radio_button_scenario_2:
-                if (checked)
-                    TCP.getInstance(MainActivity.this).SendMessageTask("road_scenario", EXTENSION_PLAN);
-
-                break;
-        }
-    }
+//    public void onRoadScenarioRadioButtonClicked(View v) {
+//        boolean checked = ((RadioButton) v).isChecked();
+//        // Check which radio button was clicked
+//        switch(v.getId()) {
+//            case R.id.radio_button_scenario_0:
+//                if (checked)
+//                    TCP.getInstance(MainActivity.this).SendMessageTask("road_scenario", NO_CLOSE_ROADS);
+//
+//                break;
+//            case R.id.radio_button_scenario_1:
+//                if (checked)
+//                    TCP.getInstance(MainActivity.this).SendMessageTask("road_scenario", PEDESTRIAN_ZONE_ACTIVE);
+//                break;
+//            case R.id.radio_button_scenario_2:
+//                if (checked)
+//                    TCP.getInstance(MainActivity.this).SendMessageTask("road_scenario", EXTENSION_PLAN);
+//
+//                break;
+//        }
+//    }
 
 
     // Button change status of display mode
@@ -224,12 +296,6 @@ public class MainActivity extends BaseActivity implements IMessageListener{
     }
 
 
-    public void onClick(View view){
-        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-        startActivity(intent);
-    }
-
-
     // Reset parameters
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -242,9 +308,9 @@ public class MainActivity extends BaseActivity implements IMessageListener{
                 builder.setPositiveButton(R.string.ok, (dialog, id) -> {
                     mSeekBarNumCars.setProgress(0);
                     mSeekBarNumMotorbikes.setProgress(0);
-                    mRadioGroupRoadScenario.check(R.id.radio_button_scenario_0);
+//                    mRadioGroupRoadScenario.check(R.id.radio_button_scenario_0);
                     mRadioGroupDisplayMode.check(R.id.radio_button_traffic);
-                    onRoadScenarioRadioButtonClicked(findViewById(R.id.radio_button_scenario_0));
+//                    onRoadScenarioRadioButtonClicked(findViewById(R.id.radio_button_scenario_0));
                     onDisplayModeRadioButtonClicked(findViewById(R.id.radio_button_traffic));
                 });
                 builder.setNegativeButton(R.string.cancel, (dialog, id) -> {
@@ -277,4 +343,19 @@ public class MainActivity extends BaseActivity implements IMessageListener{
     }
 
 
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        setBounder();
+
+        mMap.setOnMapClickListener(latLng -> {
+            MarkerOptions markerOptions = new MarkerOptions().position(latLng);
+            Marker marker = mMap.addMarker(markerOptions);
+
+            latLngList.add(latLng);
+            markerList.add(marker);
+        });
+
+    }
 }
